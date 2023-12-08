96e6922240f7d225c8054f3229431f86236f1fc9
mferino <mdference@gmail.com>
2023-12-08 11:43:28 -0600

    initial react app wired up

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 // constants
 const PORT = 4001;
@@ -9,6 +10,7 @@ const commentsByPostId = {};
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 // constants
 const PORT = 4000;
@@ -8,6 +9,7 @@ const PORT = 4000;
 // init app
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // mocking db
 const posts = {};

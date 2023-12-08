028090055acc3f2cf2870d582308e26c27dda1a3
mferino <mdference@gmail.com>
2023-12-08 16:11:11 -0600

    moderation service hooked up - begin testing

--- moderation/index.js
+++ moderation/index.js
@@ -9,8 +9,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post("events", (req, res) => {
+app.post("/events", async (req, res) => {
+    const { type, data } = req.body;
 
+    if (type === "CommentCreated") {
+        const status = data.content.include("orange") ? "rejected" : "approved";
+    }
+
+    await axios.post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+            id: data.id,
+            postId: data.postId,
+            status,
+            content: data.content,
+        }
+    });
+
+    req.send({});
 });
 
 app.listen(PORT, () => {

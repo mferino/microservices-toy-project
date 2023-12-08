df65d872f182f917b4b2ecfa3fe59a8c878b4b7e
mferino <mdference@gmail.com>
2023-12-08 13:10:11 -0600

    event bus emitting bruh

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 // constants
 const PORT = 4001;
@@ -16,15 +17,24 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const { content } = req.body;
-    
+
     const comments = commentsByPostId[req.params.id] || [];
 
     comments.push({ id: commentId, content });
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post("http://localhost:4005/events", {
+        type: "commentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+        }
+    });
 
     res.status(201).send(comments);
 });

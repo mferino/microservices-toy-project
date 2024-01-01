4abda19bee7be03b1959c675107664ff360a7782
mferino <mdference@gmail.com>
2024-01-01 17:26:19 -0600

    updated urls in event-bus index file

--- event-bus/index.js
+++ event-bus/index.js
@@ -20,20 +20,20 @@ app.post("/events", (req, res) => {
         console.log(err.message);
     });
 
-    // // Comments
-    // axios.post("http://localhost:4001/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    // Comments
+    axios.post("http://comments-srv:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
-    // // Query
-    // axios.post("http://localhost:4002/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    // Query
+    axios.post("http://query-srv:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
-    // // Moderation
-    // axios.post("http://localhost:4003/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    // Moderation
+    axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: "OK" });
 });

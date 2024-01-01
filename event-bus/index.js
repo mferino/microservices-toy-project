b849bcd212ba190b9d53f493a6cc5fd8a474562e
mferino <mdference@gmail.com>
2024-01-01 16:47:55 -0600

    updating comments to use service url

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,24 +16,24 @@ app.post("/events", (req, res) => {
     events.push(event);
 
     // Posts
-    axios.post("http://localhost:4000/events", event).catch((err) => {
+    axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
         console.log(err.message);
     });
 
-    // Comments
-    axios.post("http://localhost:4001/events", event).catch((err) => {
-        console.log(err.message);
-    });
+    // // Comments
+    // axios.post("http://localhost:4001/events", event).catch((err) => {
+    //     console.log(err.message);
+    // });
 
-    // Query
-    axios.post("http://localhost:4002/events", event).catch((err) => {
-        console.log(err.message);
-    });
+    // // Query
+    // axios.post("http://localhost:4002/events", event).catch((err) => {
+    //     console.log(err.message);
+    // });
 
-    // Moderation
-    axios.post("http://localhost:4003/events", event).catch((err) => {
-        console.log(err.message);
-    });
+    // // Moderation
+    // axios.post("http://localhost:4003/events", event).catch((err) => {
+    //     console.log(err.message);
+    // });
 
     res.send({ status: "OK" });
 });
